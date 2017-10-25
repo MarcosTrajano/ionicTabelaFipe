8fd89990dfce00ea401a9ff071a7e2123eb22527
MarcosTrajano <marcos.trajano.mt@gmail.com>
2017-10-24 21:47:48 -0300

    mudanca no estilo da pagina valor-carro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { Http, HttpModule } from '@angular/http';
+import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,6 +1,6 @@
 import { CarrosPage } from './../carros/carros';
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
@@ -9,14 +9,33 @@ import 'rxjs/add/operator/map';
   templateUrl: 'list.html'
 })
 export class ListPage {
-  public marcas:Array<string>;
+  public marcas: Array<string>;
   private url:string = "http://fipeapi.appspot.com/api/1/carros/marcas.json"
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public loadingCtrl: LoadingController) {
+    this.fetchContent();
+  }
+
+  fetchContent (){
+    let loading = this.loadingCtrl.create({
+      content:'Fetching content...'
+    });
+
+    loading.present();
+    
     this.http.get(this.url).map(res => res.json()).subscribe(data => {
-        this.marcas = data
-      }
-    )
+      this.marcas = data
+      loading.dismiss();
+    });
+  }
+
+  filterItems(ev){
+    // var val = ev.target.value;
+    // if(val && val.trim() != ''){
+    //   this.marcas = this.marcas.filter((item) =>{
+    //     return item.data.name.toLowerCase().indexOf(val.toLowerCase()) > -1;
+    //   })
+    // }
   }
 
   carros(id: number) {
